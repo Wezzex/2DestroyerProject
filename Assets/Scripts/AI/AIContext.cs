b3998387641d01c3e4ff56fe252eb8c7e3b9b940
Marcus Wessman <44946057+Wezzex@users.noreply.github.com>
2026-03-20 15:25:52 +0100

    Not Mutch

---
+++ Assets/Scripts/AI/AIContext.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public abstract class AIContext 
+{
+    ShipController shipController;
+    AIDetector detector;
+    UnitManager unitManager;
+    Transform selfTransform;
+}
