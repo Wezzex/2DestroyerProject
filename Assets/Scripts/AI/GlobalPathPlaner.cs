729417cbf8a156eef102728a93dff084c27e461e
Marcus Wessman <44946057+Wezzex@users.noreply.github.com>
2026-04-08 17:41:29 +0200

    Bezier

--- Assets/Scripts/AI/GlobalPathPlaner.cs
+++ Assets/Scripts/AI/GlobalPathPlaner.cs
@@ -1,16 +1,19 @@
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.Splines;
 
 public class GlobalPathPlaner : MonoBehaviour
 {
     private Transform startTransform;
     private Transform endTransform;
 
-    private Vector3[] corners;
+    private Transform[] Points;
     private float pathLength;
 
     public NavMeshAgent agent;
     public NavMeshPath path;
+
+    [SerializeField] private float SmoothingLength;
 
     private void Start()
     {
@@ -18,8 +21,55 @@ public class GlobalPathPlaner : MonoBehaviour
         path = GetComponent<NavMeshPath>();
     }
 
-    private void Update()
+    private float PathLength(NavMeshPath path)
+    {
+        if (path.corners.Length < 2)
+        {
+            return 0;
+        }
+
+        float lengthSoFar = 0.0f;
+        for (int i = 0; i < path.corners.Length; i++)
+        {
+            lengthSoFar += Vector3.Distance(path.corners[i - 1], path.corners[i]);
+        }
+
+        return lengthSoFar;
+
+    }
+
+    public void SetDestination(Vector3 position)
     {
         NavMesh.CalculatePath(startTransform.position, endTransform.position, NavMesh.AllAreas, path);
+        if (Points.Length > 2)
+        {
+            BezierCurve[] bezierCurves = new BezierCurve[Points.Length - 1];
+        }
+    }
+
+    private void SmoothCurve(BezierCurve[] curves, Vector3[] corners)
+    {
+        for (int i = 0; i < curves.Length; i++)
+        {
+            if (curves[i] == null)
+            {
+                curves[i] = new BezierCurve();
+            }
+
+            Vector3 position = corners[i];
+            Vector3 lastPosition = i == 0 ? corners[i] : corners[i - 1];
+            Vector3 nextPosition = corners[i + 1];
+
+            Vector3 lastDirection = (position - lastPosition).normalized;
+            Vector3 nextDirection = (nextPosition - position).normalized;
+
+            Vector3 startTangent = (lastDirection + nextDirection) * SmoothingLength;
+            Vector3 endTangent = (nextDirection + lastDirection) * -1 * SmoothingLength;
+        }
+    }
+
+    private void Update()
+    {
+        
     }
 }
